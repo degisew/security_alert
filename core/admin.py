2e42449b38bc91556e19d4a4dff283284529984b
Degisew <jegisew21@gmail.com>
2023-11-28 11:14:49 +0300

    app init

---
+++ core/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django_neomodel import admin as neo_admin
+# from .models import NeoUser
+
+# class UserAdmin(admin.ModelAdmin):
+#     list_display = ['email']
+
+# neo_admin.register(NeoUser, UserAdmin)
